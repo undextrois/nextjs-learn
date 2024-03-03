7d0e75f0029022fe3b23e9376e2f02ff1ec5f656
undextrois <undextrois@gmail.com>
2024-03-03 00:34:57 -0500

    Update page.tsx
    */}

--- app/page.tsx
+++ app/page.tsx
@@ -7,10 +7,10 @@ import { lusitana } from '@/app/ui/fonts';
 
 export default function Page() {
   return (
-    //These are Tailwind classes:
+     {/* These are Tailwind classes */}
     <main className="flex min-h-screen flex-col p-6">
       <div className="flex h-20 shrink-0 items-end rounded-lg bg-blue-500 p-4 md:h-52">
-        // ...
+      
         {/* <AcmeLogo /> */}
       </div>
       <div className="mt-4 flex grow flex-col gap-4 md:flex-row">
